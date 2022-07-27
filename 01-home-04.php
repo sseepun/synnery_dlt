13ec86fb6606994ddca24a4da852cd8a20926db1
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-07-28 06:45:33 +0700

    update margin

--- 01-home-04.php
+++ 01-home-04.php
@@ -393,21 +393,21 @@
           <div class="grid lg-50 mt-0">
             <div class="text-container">
               <h6 class="color-05">แบบสำรวจความคิดเห็น</h6> 
-              <p class="mt-2">ท่านมีความคิดเห็นเกี่ยวกับประโยชน์ของข้อมูลและสารสนเทศที่เผยแพร่ทาง <br>
+                <p class="mt-2">ท่านมีความคิดเห็นเกี่ยวกับประโยชน์ของข้อมูลและสารสนเทศที่เผยแพร่ทาง <br>
                   เว็บไซต์ของสำนักงานขนส่งจังหวัดสมุทรปราการอย่างไร</p>
-                  <fieldset class="d-flex mt-3">
-                    <?php foreach(['พอใช้', 'ปานกลาง', 'มาก', 'มากที่สุด'] as $i=>$d){?>
-                      <div class="rc-container">
-                        <input type="radio" id="poll_<?= $i ?>" name="poll" value="<?= $i ?>" />
-                        <label for="poll_<?= $i ?>"><?= $d ?></label>
-                      </div>
-                    <?php }?>
-                </fieldset>  
+                <fieldset class="d-flex mt-3">
+                  <?php foreach(['พอใช้', 'ปานกลาง', 'มาก', 'มากที่สุด'] as $i=>$d){?>
+                    <div class="rc-container">
+                      <input type="radio" id="poll_<?= $i ?>" name="poll" value="<?= $i ?>" />
+                      <label for="poll_<?= $i ?>"><?= $d ?></label>
+                    </div>
+                  <?php }?>
+              </fieldset>  
               <div class="btns d-flex mt-5" data-aos="fade-up" data-aos-delay="300">
                 <a href="#" class="btn btn-action style-05 btn-05">
                   <span>ทำแบบสำรวจ</span> 
                 </a>
-            </div>
+              </div>
             </div>   
           </div>     
         </div>    
