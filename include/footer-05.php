b4109f1a75daa7b45cb0379a8b0753b4bb72ec33
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-08-01 15:22:10 +0700

    update footer style-05

--- include/footer-05.php
+++ include/footer-05.php
@@ -1,45 +1,34 @@
-
-
 <!-- Back to Top -->
-<a class="back-to-top style-01 active" href="#">
+<a class="back-to-top style-05 active" href="#">
   <em class="fas fa-chevron-up"></em>
 </a>
 
 <!-- Footer -->
 <nav class="footer style-05">
-  <img class="pattern" src="public/assets/app/images/bg/pattern-03.png" alt="Pattern" />
+  <img class="img-bg" src="public/assets/app/images/bg/pattern-03.png" alt="Pattern" />
   <div class="container">
     <div class="grids">
-      <div class="grid lg-50 sm-100 mt-6">
-        <div class="d-flex mt-6">
-          <img src="public/assets/app/images/logo-01.png" width="40" height="40" alt="Website Logo" />
-           <div>
-            <div class="ml-2">
-              <h6 class="fw-300 lh-3xs letter-spacing xs">สำนักงานขนส่งจังหวัดสมุทรปราการ</h6>
-              <p class="xs fw-100 lh-3xs">
-                  กรมการขนส่งทางบก
-               </p>
-               <p class="sm fw-100 mt-2">
-                  1332 หมู่ที่ 3 ตำบลบางปูใหม่ อำเภอเมือง จังหวัดสมุทรปราการ 10280
-               </p>
-                <div class="grid sm-100 footer-copyright">
-                  <p class="sm mt-3">
-                    <a class="fw-100">
-                    ลิขสิทธิ์ 2565 : กรมการขนส่งทาง
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
+      <div class="grid lg-60 md-100 sm-100">
+        <a class="logo" href="#">
+          <img src="public/assets/app/images/logo-01.png" alt="Website Logo" />
+          <div class="text-wrapper">
+            <h6 class="fw-300 lh-2xs">สำนักงานขนส่งจังหวัดสมุทรปราการ</h6>
+            <p class="fw-100 lh-sm sm">
+              กรมการขนส่งทางบก
+            </p>
+            <p class="fw-100 mt-1">1332 หมู่ที่ 3 ตำบลบางปูใหม่ อำเภอเมือง จังหวัดสมุทรปราการ 10280</p>
           </div>
-        </div>
-        <div class="grid lg-50 sm-100 mt-2">
-          <div class="d-flex ai-center jc-center h-full">
-            <p class="xs fw-100">ผู้เยี่ยมชมวันนี้ <span class="h4 ml-1">831</span></p>
-            <p class="xs fw-100 ml-3">ผู้เยี่ยมชมทั้งหมด <span class="h4 ml-1">942,675</span></p>
-          </div>
-        </div>
+        </a>
+      </div>
+      <div class="grid lg-40 md-100 sm-100">
+        <div class="footer-middle d-flex ai-end jc-center h-full">
+          <p class="sm fw-100">ผู้เยี่ยมชมวันนี้ <span class="h4 fw-500 ml-2">831</span></p>
+          <p class="sm fw-100 ml-2">ผู้เยี่ยมชมทั้งหมด <span class="h4 fw-500 ml-2">942,675</span></p>
+        </div>  
       </div>
     </div>
-    <div class="footer-line"></div>
+    <div class="footer-bottom">
+      <p class="fw-100">ลิขสิทธิ์ 2565 : กรมการขนส่งทางบก</p>
+    </div>
+  </div>
 </nav>
