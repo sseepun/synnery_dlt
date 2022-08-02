4e88f6e06c4b09c1f8183d276b2ab8b4b7dd5992
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-08-02 16:03:03 +0700

    update ss-card-22

--- minisite-01-home-03.php
+++ minisite-01-home-03.php
@@ -28,28 +28,38 @@
       <h3 class="text-center color-03">ข่าวสารประชาสัมพันธ์</h3>
       <div class="tab-container">
         <div class="grids">
-          <div class="grid lg-20 md-30">
-            <h5 class="color-06 fw-600">ประเภท</h5>
+          <div class="grid lg-20 md-100 sm-100">
+            <div class="d-flex ai-center">
+              <div class="icon-bars">
+                <img src="public/assets/app/images/icon/54.png" alt="Icon" />
+              </div>
+              <h5 class="color-06 fw-600 ml-2">ประเภท</h5>
+            </div>
             <div class="tabs tabs-04 mt-4" data-aos="fade-up" data-aos-delay="150">
               <div class="wrapper">
                 <?php foreach($categories as $i=>$d){?>
                   <div class="tab <?= $i==0? 'active': '' ?>" data-tab="content_<?= $i ?>">
-                    <p class="lg color-14 fw-400"><?= $d['title'] ?></p>
+                    <p class="lg fw-400"><?= $d['title'] ?></p>
                     <div class="ss-sep-03 bg-15"></div>
                   </div>
                 <?php }?>
               </div>
             </div>
-            <a class="p d-block text-center" href="#">อ่านทั้งหมด</a>
+            <div class="d-flex ai-center jc-center btn-more mt-2">
+              <a class="p d-block text-center" href="#">อ่านทั้งหมด</a> 
+              <div class="icon ml-2">
+                <em class="fa-solid fa-angle-right"></em>
+              </div>
+            </div>
          </div>
-          <div class="grid lg-80 md-70">
+          <div class="grid lg-80 md-100 sm-100">
             <div class="grids">
-              <div class="grid lg-50 mt-0">
+              <div class="grid lg-50 md-100 sm-100 mt-0">
                 <div class="tab-contents mt-2" data-aos="fade-up" data-aos-delay="300">
                   <?php foreach($categories as $i=>$d){?>
                     <div class="tab-content <?= $i==0? 'active': '' ?>" data-tab="content_<?= $i ?>">
                       <?php for($j=0; $j<1; $j++){?>
-                        <div class="ss-card ss-card-21">
+                        <div class="ss-card ss-card-22">
                           <div class="ss-img vertical-2">
                             <div class="img-bg" style="background-image:url('public/assets/app/images/content/5<?= ($i+$j+1)%5+5 ?>.jpg');"></div>
                           </div>
@@ -59,31 +69,40 @@
                   <?php }?>
                 </div>  
               </div> 
-              <div class="grid lg-50 mt-0">
-                <div class="tab-contents" data-aos="fade-up" data-aos-delay="0">
+              <div class="grid lg-50 md-100 sm-100">
+                <div class="tab-contents pl-5" data-aos="fade-up" data-aos-delay="0">
                   <?php foreach($categories as $i=>$d){?>
                     <div class="tab-content <?= $i==0? 'active': '' ?>" data-tab="content_<?= $i ?>">
-                      <div class="ss-tags">
-                        <h3 class="tag fw-300 lh-xs bg-03"><?= $d['title'] ?></h3>
-                      </div>
-                      <div class="d-flex ai-center mt-2">
-                        <div class="date">
-                          <p>17 กุมภาพันธ์ 2565</p>
+                      <div class="ss-card ss-card-22">
+                        <div class="ss-tags">
+                          <h3 class="tag lh-xs bg-03"><?= $d['title'] ?></h3>
                         </div>
-                        <div class="icon ml-6 pl-6">
-                          <img src="public/assets/app/images/icon/53.png" alt="Icon" /> 
-                          <span class="color-16 ml-1">200</span>
+                        <div class="d-flex ai-center mt-2">
+                          <div class="date">
+                            <p>17 กุมภาพันธ์ 2565</p>
+                          </div>
+                          <div class="icon ml-6 pl-6">
+                            <img src="public/assets/app/images/icon/53.png" alt="Icon" /> 
+                            <span class="color-16 ml-1">200</span>
+                          </div>
                         </div>
+                        <div class="text-container">
+                          <h5 class="fw-400">
+                            กรมการขนส่งทางบก เผย!! มีผู้ให้บริการรถยนต์รับจ้างผ่านระบบแอปพลิเคชันผ่าน...
+                          </h5>
+                          <p class="desc lg color-16 mt-2">
+                            ตามที่นายศักดิ์สยาม​ ชิดชอบ รัฐมนตรีว่าการกระทรวงคมนาคม ได้มี
+                            นโยบายให้กระทรวงคมนาคมออกกฎกระทรวงรถยนต์รับจ้างผ่านระบบอิเล็กทรอนิกส์ พ.ศ. 2564
+                          </p>
                       </div>
-                      <h5 class="fw-400">
-                        กรมการขนส่งทางบก เผย!! มีผู้ให้บริการรถยนต์รับจ้างผ่านระบบแอปพลิเคชันผ่าน...
-                      </h5>
-                      <p class="lg color-16 mt-2">
-                        ตามที่นายศักดิ์สยาม​ ชิดชอบ รัฐมนตรีว่าการกระทรวงคมนาคม ได้มี
-                        นโยบายให้กระทรวงคมนาคมออกกฎกระทรวงรถยนต์รับจ้างผ่านระบบอิเล็กทรอนิกส์ พ.ศ. 2564
-                      </p>
+                      </div>
                     </div>
                   <?php }?>
+                  <div class="btns d-flex mt-6">
+                    <a href="#" class="btn btn-action style-03 btn-08">
+                      อ่านต่อ
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -109,9 +128,17 @@
                         <div class="ss-tags">
                           <div class="tag no-br ws-nowrap"><?= $d['title'] ?></div>
                         </div>
-                        <a class="title p lg fw-400 lh-sm" href="#">
-                          กรมการขนส่งทางบก ส่งความสุขและความปรารถนาดี ในช่วงเทศกาลตรุษจีน
-                          กรมการขนส่งทางบก ส่งความสุขและความปรารถนาดี ในช่วงเทศกาลตรุษจีน
+                        <div class="d-flex ai-center mt-2">
+                          <div class="date">
+                            <p>17 กุมภาพันธ์ 2565</p>
+                          </div>
+                          <div class="icon ml-6 pl-6">
+                            <img src="public/assets/app/images/icon/53.png" alt="Icon" /> 
+                            <span class="color-16 ml-1">200</span>
+                          </div>
+                      </div>
+                        <a class="title p lg fw-300 lh-sm mt-2" href="#">
+                           กรมการขนส่งทางบก เตือนภัย!!! ระวังมิจฉาชีพหลอกทำใบขับขี่ปลอมบน
                         </a>
                       </div>
                       </div>
@@ -125,6 +152,8 @@
       </div>
     </div>
   </section>
+
+  
    
   <?php // include_once('include/footer-04.php'); ?>
   <?php include_once('include/script.php'); ?>
